feaa445fe990ac8e1dab0d4abd3a762ad2614fe6
Andreazo Silva <andreazocoisas@gmail.com>
2020-03-26 10:50:27 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,21 +1,24 @@
 const socketClient = require('socket.io-client');
 const userID = ''
 const jwt_token = ''
+
 function connectServer() {
+    
     const io = socketClient.connect('http://127.0.0.1:3333', {
         query: `token=${jwt_token}`
     })
+    
+    /*Connect to a unique "room" by some key*/
     io.emit('connectRoom', userID);
+    
+    /*Listen some error*/
     io.on("error", function (error) {
         console.log(error)
     })
-
-    io.on('some_url_to_listen_from_server', data =>{
+    
+    io.on('some_url_to_listen_from_server_to_receive_data', data =>{
         console.log(data)
     })
-
-    return io
 }
 
-  
 connectServer()
